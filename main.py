56f9b870e80d613a6dc9238826330844ba58bba5
xxppvvvnn2-dot <xxppvvvnn2@gmail.com>
2026-01-13 23:32:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# This is a sample Python script.
+print('ادخل نقاط الطلاب ')
+n = []
+for i in range(5):
+    score = float(input(f'ادخلها هنا :{i + 1}'))
+    n.append(score)
+print('اعلى درجة',max(n))
+print('اقل درجة ',min(n))
+print('مجموع الدرجات', sum(n))
+def u():
+    print("ادخل درجة للحصول على تقدير الطالب")
+    for i in range(5):
+        o = float(input('ادخله هنا:'))
+        if o > 100 or o < 0:
+            print("القيمة خاطئة")
+        elif o >= 90:
+            print('التقدير العام ممتاز')
+        elif o >= 80:
+            print('التقدير العام جيد جدا')
+        elif o >= 65:
+            print("التقدير العام جيد")
+        elif o >= 55:
+            print("التقدير العام مقبول")
+        elif o >= 50:
+            print("التقدير العام ناج بالكاد")
+        else:
+            print("التقدير العام راسب")
+u()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
